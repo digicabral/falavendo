7915e53115a8f94426bb00a6b4e51144b4a82e5d
Rodrigo Cabral <digicabral@gmail.com>
2026-02-05 14:38:06 -0300

    feat: improve cards for no image

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,20 @@
 import "./Card.css";
 
 const Card = ({ item, onCardSelect, isSelected }: any) => {
+  const hasImage = Boolean(item.image);
+
   return (
     <button
-      className={`card-container ${isSelected ? "selected" : ""}`}
+      className={`card-container ${isSelected ? "selected" : ""} ${
+        !hasImage ? "no-image" : ""
+      }`}
       onClick={() => onCardSelect(item)}
       type="button"
     >
-      <img src={item.image} alt={item.label} className="card-image" />
+      {hasImage && (
+        <img src={item.image} alt={item.label} className="card-image" />
+      )}
+
       <span className="card-label">{item.label}</span>
     </button>
   );
